a87d87f557a6627911c34b06af9c4c7e4cfe7277
arricodyanto <arricohandyanto@gmail.com>
2021-11-17 22:33:10 +0700

    update server.js

--- server.js
+++ server.js
@@ -3,24 +3,15 @@ const errorHandler = require('./middlewares/error-handler')
 const app = express()
 const port = process.env.PORT || 3000
 const routers = require('./routes')
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`) // Example for postgres
-
 
 app.use(express.json())
 app.use(routers)
 
-app.get('/', async(req, res) => {
-    // res.json({
-    //     success: true,
-    //     message: "Hello, World!"
-    // })
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "Hello, World!"
+    })
 })
 
 app.use(errorHandler)
